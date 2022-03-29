146a2c14f1ab46e685f61831382220663f99e4d5
p4yl0ad <57832389+p4yl0ad@users.noreply.github.com>
2022-03-29 20:58:23 +0100

    Update setup.sh

--- helpers/setup.sh
+++ helpers/setup.sh
@@ -2,7 +2,9 @@ DEBIAN_FRONTEND=noninteractive sudo apt-get update -y && sudo apt-get upgrade -y
 DEBIAN_FRONTEND=noninteractive sudo apt install python3-pip mysql-server libmysqlclient-dev python3-flask -y
 pip3 install -r requirements.txt
 sudo bash -c 'sudo mysql < "helpers/db/init.sql"'
-FLASK_APP=cvd_app FLASK_ENV=development flask run
+
+
+#PYTHONDONTWRITEBYTECODE=1 FLASK_APP=cvd_app FLASK_ENV=development python3 -m flask run
 
 #find . -name __pycache__ -exec rm -rf {} \;
 #python3 -m pytest -vv
